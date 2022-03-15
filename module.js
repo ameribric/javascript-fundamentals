47c5119f901c19ddde3d1845e797da0b761d53d7
Amer Ibric <ameribric@gmail.com>
2022-03-15 15:45:17 -0400

    Added js module file

--- module.js
+++ module.js
@@ -1,8 +1,12 @@
+
+function quiz(){
 let myName = prompt('Enter your name')
 let month = prompt('What month were you born')
 let food = prompt('What is your favorite food')
 
 
-console.log('My name is,' myName)
-console.log('I was born in,' month)
-console.log('My favorite food is,' food)
+console.log('My name is', myName)
+console.log('I was born in', month)
+console.log('My favorite food is', food)
+}
+quiz()
