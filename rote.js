b9af050efe7d0faa99cc3edbd682af7c665d00c7
Amer Ibric <ameribric@gmail.com>
2022-03-16 13:31:17 -0400

    Added js module file

---
+++ rote.js
@@ -0,0 +1,16 @@
+const greet = () => {
+    console.log('Hello,world')
+}
+
+greet()
+
+const square = () => {
+    return num * num
+}
+square(25)
+
+const greetByName = (str) => {
+console.log('Hello', str)
+}
+
+greetByName('amer')
