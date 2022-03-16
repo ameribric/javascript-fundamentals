e69a2e83f928af7195573e882107b68a6c5ee487
Amer Ibric <ameribric@gmail.com>
2022-03-16 17:42:07 -0400

    Added js module file

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-console.log('JavaScript is running!')
-let score = 100
+// console.log('JavaScript is running!')
+// let score = 100
 
 
-const greet = () => {
-    console.log('Hello, world!')
+// const greet = () => {
+//     console.log('Hello, world!')
+// }
+// greet()
+
+// const square = (num) => {
+//     return num * num
+// }
+// square(10)
+
+// const greetByName = (str) => {
+//     console.log('Hello,', str)
+// }
+// greetByName('Amer Ibric')
+
+
+// const multiply = (n1, n2) => {
+//     console.log("Multiplying", n1, "by", n2)
+//     return n1 * n2
+// }
+
+// return double = (n) => {
+//     return n * 2
+// }
+// double(multiply(2,4))
+
+
+// let array1 = ['a', 'b', 'c', 'd', 'e']
+// let array2 = [1, ,2, 3, 4, 5, 6, 7]
+
+// const determineLength = (arr) => {
+// if (arr.length > 5) {
+//     return 'This array is long'
+// } else {
+//     return 'This array is short'
+// }
+// }
+
+const askForString = () => {
+    let str = prompt('Enter a string')
+    return str;
 }
-greet()
 
-const square = (num) => {
-    return num * num
-}
-square(10)
+console.log(askForString().split(''))
 
-const greetByName = (str) => {
-    console.log('Hello,', str)
-}
-greetByName('Amer Ibric')
