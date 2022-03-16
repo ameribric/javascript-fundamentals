208edf2e8d376fd8b3f64a63e5dbb471717ea64d
Amer Ibric <ameribric@gmail.com>
2022-03-16 12:09:59 -0400

    Added js module file

--- module.js
+++ module.js
@@ -1,12 +1,43 @@
 
-function quiz(){
+
+
+const quiz = () => {
 let myName = prompt('Enter your name')
 let month = prompt('What month were you born')
 let food = prompt('What is your favorite food')
-
-
 console.log('My name is', myName)
 console.log('I was born in', month)
 console.log('My favorite food is', food)
 }
-quiz()
+
+const print = () => {
+    console.log('I love to print text')
+}
+
+const functionA = () => {
+    console.log('FUNCTION A HAS BEEN CALLED!')
+    functionB()
+}
+
+const functionB = () => {
+    console.log('FUNCTION B HAS BEEN CALLED!')
+}
+
+const functionC = () => {
+    console.log('FUNCTION C HAS BEEN CALLED!')
+}
+
+const greet = (person) => {
+    console.log('HELLO!!!', person)
+}
+
+
+const blender = (string) => {
+return string.split('')
+}
+
+
+const square = (num, message) => {
+console.log('Message:', message)
+    return num * num
+}
